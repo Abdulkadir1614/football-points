bc686db5932eedd735bd73a356fef25c226c10d9
Abdulkadir1614 <abdukadir1614@gmail.com>
2024-09-07 15:23:56 +0800

    Done

--- challenge.js
+++ challenge.js
@@ -1,2 +1,13 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function calculatePoints (wins, draws, losses){
+    const PerWin = 3;
+    const PerDraw = 1;
+    const PerLoss = 0;
+
+
+    const totalPoints = (wins * PerWin)+(draws * PerDraw) + (losses * PerLoss);
+    console.log(`The Total Points : ${totalPoints}`);
+}
+calculatePoints(5, 3 , 2)
